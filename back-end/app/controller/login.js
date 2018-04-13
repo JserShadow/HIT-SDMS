24bb25c75810460b444433b427ba387596d3d1e2
JserShadow <424108968@qq.com>
2018-04-13 21:51:37 +0800

    repair bugs

--- back-end/app/controller/login.js
+++ back-end/app/controller/login.js
@@ -34,11 +34,8 @@ class LoginController extends Controller {
   }
   async getUserInfo() {
     const value = this.ctx.request.body.value;
-    console.log(this.ctx.request.body);
     const { Users } = this.ctx.model;
     const mongoRes = await Users.find({ openId: value });
-    console.log(1);
-    console.log(mongoRes);
     this.ctx.body = mongoRes;
   }
 }
--- back-end/app/service/login.js
+++ back-end/app/service/login.js
@@ -6,9 +6,7 @@ class LoginService extends Service {
   async addUser(wxUserInfo) {
     const { openId, nickName, avatarUrl, gender, city, country, province, language } = wxUserInfo;
     const findRes = await this.ctx.model.Users.find({ openId });
-    console.log(findRes);
     if (findRes.length === 0) {
-      console.log('save');
       const newUser = new this.ctx.model.Users({ openId, nickName, avatarUrl, gender, city, country, province, language });
       await newUser.save();
     } else {
