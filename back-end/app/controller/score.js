e976faf3a3445c1b4e923c23b92efef365935e5c
JserShadow <424108968@qq.com>
2018-05-19 13:31:56 +0800

    score sort && bkg pic && title

--- back-end/app/controller/score.js
+++ back-end/app/controller/score.js
@@ -6,7 +6,19 @@ class ScoreController extends Controller {
   async getAllScores() {
     const { openId } = this.ctx.request.body;
     const { Score } = this.ctx.model;
-    const res = await Score.find({ openId });
+    const res = await Score.find({ openId }).sort({'gradeName': 1});
+    for (let i = 1; i < res.length; i++) {
+      if (!res[1]) {
+        break;
+      }
+      if (res[i].gradeName.year === res[i-1].gradeName.year) {
+        if (res[i].gradeName.term === '春季学期' && res[i-1].gradeName.term === '夏季学期') {
+          let a = res[i];
+          res[i] = res[i-1];
+          res[i-1] = a;
+        }
+      }
+    }
     this.ctx.body = {
       message: 'ok',
       scores: res,
