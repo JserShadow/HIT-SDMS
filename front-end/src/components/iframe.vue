d55d1696111d748c67fe869417a9d49de71dfca5
Shadow_ZXJ <424108968@qq.com>
2018-06-12 12:03:35 +0800

    dialog

--- front-end/src/components/iframe.vue
+++ front-end/src/components/iframe.vue
@@ -3,11 +3,11 @@
   <iframe style="width: 180vw;height: 200vh;" :src="transcript" frameborder="0"></iframe>
   <van-dialog
     v-model="show"
-    style="word-wrap: break-all"
+    
   >
     <p>该页面为学生成绩单预览</p>
     <p>PC端查看请长按复制以下网址:</p>
-    <p>{{transcript}}</p>
+    <p style="padding: 0 20px;word-wrap: break-word;">{{transcript}}</p>
   </van-dialog>
 </div>
 </template>
